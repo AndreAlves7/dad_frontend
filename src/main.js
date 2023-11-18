cae9e79f8d5a998268a81c37c0618248523fb31b
Joao Tavares <2212468@my.ipleiria.pt>
2023-11-18 20:05:19 +0000

    Add environment configuration files and remove unused config.js file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import config from './utils/config'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
 
-const apiDomain = config.baseAPI
+ const apiDomain = import.meta.env.VITE_API_DOMAIN
+
 // const apiDomain = import.meta.env.VITE_API_DOMAIN
 // const wsConnection = import.meta.env.VITE_WS_CONNECTION
 
