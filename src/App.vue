a99fb011f4c7224647b0e36c8232a81385448a0c
André Colaço Alves Filho <2212948@my.ipleiria.pt>
2023-12-17 22:37:18 +0000

    Icon change

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ const logout = async () => {
   <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top flex-md-nowrap p-0 shadow" v-if="userStore.user">
     <div class="container-fluid">
       <router-link class="navbar-brand col-md-3 col-lg-2 me-0 px-3" :to="{ name: 'home' }" @click="clickMenuOption">
-        <BIconRocketTakeoff/>
+        <BIconRocketTakeoff class="me-2 fs-3"/>
                 Rabolut
       </router-link>
       <button id="buttonSidebarExpandId" class="navbar-toggler" type="button" data-bs-toggle="collapse"
